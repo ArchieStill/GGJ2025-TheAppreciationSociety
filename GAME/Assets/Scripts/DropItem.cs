a5ea3c91bb4db19a275077cd1b7d91cbb9487dca
ArchieStill <archie2.still@live.uwe.ac.uk>
2025-01-25 12:39:53 +0000

    Added jelly hi nathan

--- GAME/Assets/Scripts/DropItem.cs
+++ GAME/Assets/Scripts/DropItem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DropItem : MonoBehaviour
 {
     public GameObject Boba;
+    public GameObject Jelly;
     public GameObject Spawn1;
     public GameObject Spawn2;
     public GameObject Spawn3;
@@ -12,40 +13,74 @@ public class DropItem : MonoBehaviour
     public GameObject Spawn5;
 
     private Rigidbody bobaBody;
+    private Rigidbody jellyBody;
 
     private void Start()
     {
         bobaBody = Boba.GetComponent<Rigidbody>();
+        jellyBody = Jelly.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
+            DropBoba();
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            DropJelly();
+    }
+
+
+    private void DropBoba()
+    {
+        int SpawnToChoose = (Random.Range(1, 6));
+        var force = new Vector3(Random.Range(1, 6), 1, Random.Range(1, 6));
+        bobaBody.AddForce(force);
+        switch (SpawnToChoose)
         {
-            int SpawnToChoose = (Random.Range(1, 6));
-            Debug.Log(SpawnToChoose);
-            var force = new Vector3(Random.Range(1, 6), 1, Random.Range(1, 6));
-            bobaBody.AddForce(force);
-            switch (SpawnToChoose)
-            {
-                case 1:
-                    Instantiate(Boba, Spawn1.transform.position, Quaternion.identity);
-                    break;
-                case 2:
-                    Instantiate(Boba, Spawn2.transform.position, Quaternion.identity);
-                    break;
-                case 3:
-                    Instantiate(Boba, Spawn3.transform.position, Quaternion.identity);
-                    break;
-                case 4:
-                    Instantiate(Boba, Spawn4.transform.position, Quaternion.identity);
-                    break;
-                case 5:
-                    Instantiate(Boba, Spawn5.transform.position, Quaternion.identity);
-                    break;
-                default:
-                    break;
-            }
+            case 1:
+                Instantiate(Boba, Spawn1.transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(Boba, Spawn2.transform.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(Boba, Spawn3.transform.position, Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(Boba, Spawn4.transform.position, Quaternion.identity);
+                break;
+            case 5:
+                Instantiate(Boba, Spawn5.transform.position, Quaternion.identity);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void DropJelly()
+    {
+        int SpawnToChoose = (Random.Range(1, 6));
+        var force = new Vector3(Random.Range(1, 6), 1, Random.Range(1, 6));
+        jellyBody.AddForce(force);
+        switch (SpawnToChoose)
+        {
+            case 1:
+                Instantiate(Jelly, Spawn1.transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(Jelly, Spawn2.transform.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(Jelly, Spawn3.transform.position, Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(Jelly, Spawn4.transform.position, Quaternion.identity);
+                break;
+            case 5:
+                Instantiate(Jelly, Spawn5.transform.position, Quaternion.identity);
+                break;
+            default:
+                break;
         }
     }
 }
