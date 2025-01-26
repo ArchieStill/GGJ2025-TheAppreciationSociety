365fc806b66a6a6b2a8dfc93eca8ee3a3fe0aed9
Simon <Simone2.Farinelli@live.uwe.ac.uk>
2025-01-26 04:23:01 +0000

    Glass has tipping mechanics

---
+++ GAME/Assets/TippingScript.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TippingScript : MonoBehaviour
+{
+    public MeshRenderer liquidRenderer;
+    public float yOffset = 0;
+    public float spillRate = 0.01f;
+    public bool spilling = false;
+
+    public float spillingOffset = 0;
+    
+    private List<GameObject> childrens = new List<GameObject>();
+    private Material liquidMat;
+    
+    void Awake()
+    {
+        childrens.Clear();
+        
+        foreach (Transform child in this.transform)
+        {
+            childrens.Add(child.gameObject);
+        }
+
+        liquidMat = liquidRenderer.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spillingOffset = 0;
+        
+        var currentFill = liquidMat.GetFloat("_GlobalFill");
+        spilling = false;
+        
+        foreach (var tippingPoint in childrens)
+        {
+            var tippingPointY = tippingPoint.transform.position.y + yOffset;
+            float tippingOffsetCurrent = (tippingPointY - currentFill) * -1;
+            
+            if (tippingPointY < currentFill)
+            {
+                spilling = true;
+                
+                if (spillingOffset < tippingOffsetCurrent)
+                {
+                    spillingOffset = tippingOffsetCurrent;
+                }
+            }
+        }
+
+        if (spilling)
+        {
+            liquidMat.SetFloat("_GlobalFill", currentFill - (spillingOffset * spillRate));
+        }
+    }
+}
